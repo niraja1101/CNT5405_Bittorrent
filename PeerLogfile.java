3be6a4131e4f6ed1dcc05d8f2ff21c0b1643fd9e
niraja1101 <niraja1101@gmail.com>
2018-04-24 19:58:20 -0400

    Update PeerLogfile.java

--- PeerLogfile.java
+++ PeerLogfile.java
@@ -29,7 +29,8 @@ import java.sql.Timestamp;
                 File file1 = new File(fName);
                 if (!file1.exists()) {
                     file1.createNewFile();
-                }}
+                }
+            }
             catch (IOException e) {
                 e.printStackTrace();
             }
@@ -38,51 +39,62 @@ import java.sql.Timestamp;
         public void initiateTcpconnection(int peer_id1, int peer_id2)
         {
             String fName= haslogfile(peer_id1);
+            BufferedWriter writetofile;
             try
             {
-                FileWriter writer = new FileWriter(fName,true);
-                BufferedWriter writetofile = new BufferedWriter(writer);
+                //FileWriter writer = new FileWriter(fName,true);
+                writetofile = new BufferedWriter(new FileWriter(fName,true));
                 writetofile.write(printdate()+": Peer "+peer_id1+" initiates a connection to Peer "+peer_id2+".");
                 writetofile.newLine();
-                writetofile.close();
+
             }
 
             catch(IOException e)
             {
                 e.printStackTrace();
+            }
+            finally {
+                writetofile.close();
             }
         }
 
         public void tcpconnected(int peer_id1, int peer_id2)
         {
             String fName= haslogfile(peer_id1);
+            BufferedWriter writetofile;
             try
             {
-                FileWriter writer = new FileWriter(fName,true);
-                BufferedWriter writetofile = new BufferedWriter(writer);
+
+                writetofile= new BufferedWriter(new FileWriter(fName,true));
                 writetofile.write(printdate()+": Peer "+peer_id1+" is connected with Peer "+peer_id2+".");
                 writetofile.newLine();
-                writetofile.close();
+
             }
             catch(IOException e)
             {
                 e.printStackTrace();
+            }
+            finally {
+                writetofile.close();
             }
         }
         public void displayPreferredNeighbors(int peer_id1, String prefpeerlist)
         {
             String fName= haslogfile(peer_id1);
+            BufferedWriter writetofile;
             try
             {
-                FileWriter writer = new FileWriter(fName,true);
-                BufferedWriter writetofile = new BufferedWriter(writer);
+                writetofile = new BufferedWriter(new FileWriter(fName,true));
                 writetofile.write(printdate()+": Peer "+peer_id1+" has the Preferred neighbours "+prefpeerlist+".");
                 writetofile.newLine();
-                writetofile.close();
+
             }
             catch(IOException e)
             {
                 e.printStackTrace();
+            }
+            finally {
+                writetofile.close();
             }
 
         }
@@ -90,34 +102,40 @@ import java.sql.Timestamp;
         public void updateOptimisticallyUnchokedNeighbor(int peer_id1, int peer_id2)
         {
             String fName= haslogfile(peer_id1);
+            BufferedWriter writetofile;
             try
             {
-                FileWriter writer = new FileWriter(fName,true);
-                BufferedWriter writetofile = new BufferedWriter(writer);
+                writetofile = new BufferedWriter(new FileWriter(fName,true));
                 writetofile.write(printdate()+": Peer "+peer_id1+" has the optimistically unchoked neighbour Peer "+peer_id2+".");
                 writetofile.newLine();
-                writetofile.close();
+
             }
             catch(IOException e)
             {
                 e.printStackTrace();
+            }
+            finally {
+                writetofile.close();
             }
 
         }
         public void peerunchoked(int peer_id1, int peer_id2)
         {
             String fName= haslogfile(peer_id1);
+            BufferedWriter writetofile;
             try
             {
-                FileWriter writer = new FileWriter(fName,true);
-                BufferedWriter writetofile = new BufferedWriter(writer);
+                writetofile = new BufferedWriter(new FileWriter(fName,true));
                 writetofile.write(printdate()+": Peer "+peer_id1+" is unchoked by Peer "+peer_id2+".");
                 writetofile.newLine();
-                writetofile.close();
+
             }
             catch(IOException e)
             {
                 e.printStackTrace();
+            }
+            finally {
+                writetofile.close();
             }
 
         }
@@ -125,17 +143,20 @@ import java.sql.Timestamp;
         public void peerchoked(int peerId1, int peerId2)
         {
             String fName= haslogfile(peerId1);
+            BufferedWriter writetofile;
             try
             {
-                FileWriter writer = new FileWriter(fName,true);
-                BufferedWriter writetofile = new BufferedWriter(writer);
+                writetofile = new BufferedWriter(new FileWriter(fName,true));
                 writetofile.write(printdate()+": Peer "+peerId1+" is choked by Peer "+peerId2+".");
                 writetofile.newLine();
-                writetofile.close();
+
             }
             catch(IOException e)
             {
                 e.printStackTrace();
+            }
+            finally {
+                writetofile.close();
             }
 
         }
@@ -143,83 +164,102 @@ import java.sql.Timestamp;
         public void receivedhavemsg(int peer_id1, int peer_id2, int pieceindex)
         {
             String fName= haslogfile(peer_id1);
+            BufferedWriter writetofile;
+
+
             try
             {
-                FileWriter writer = new FileWriter(fName,true);
-                BufferedWriter writetofile = new BufferedWriter(writer);
+                writetofile = new BufferedWriter(new FileWriter(fName,true));
                 writetofile.write(printdate()+": Peer "+peer_id1+" received the 'have' message from Peer "+peer_id2+" for the piece "+pieceindex+".");
                 writetofile.newLine();
-                writetofile.close();
+
             }
             catch(IOException e)
             {
                 e.printStackTrace();
+            }
+            finally {
+                writetofile.close();
             }
 
         }
         public void receivedinterestedmsg(int peer_id1, int peer_id2)
         {
             String fName= haslogfile(peer_id1);
+            BufferedWriter writetofile;
             try
             {
-                FileWriter writer = new FileWriter(fName,true);
-                BufferedWriter writetofile = new BufferedWriter(writer);
+                writetofile = new BufferedWriter(new FileWriter(fName,true));
                 writetofile.write(printdate()+": Peer "+peer_id1+" received the 'interested' message from Peer "+peer_id2+".");
                 writetofile.newLine();
-                writetofile.close();
+
             }
             catch(IOException e)
             {
                 e.printStackTrace();
+            }
+            finally {
+                writetofile.close();
             }
         }
         public void receivednotinterestedmsg(int peer_id1, int peer_id2)
         {
             String fName= haslogfile(peer_id1);
+            BufferedWriter writetofile;
             try
             {
-                FileWriter writer = new FileWriter(fName,true);
-                BufferedWriter writetofile = new BufferedWriter(writer);
+                writetofile= new BufferedWriter(new FileWriter(fName,true));
                 writetofile.write(printdate()+": Peer "+peer_id1+" received the 'not interested' message from Peer "+peer_id2+".");
                 writetofile.newLine();
-                writetofile.close();
+
             }
             catch(IOException e)
             {
                 e.printStackTrace();
+            }
+            finally {
+                writetofile.close();
             }
         }
         public void pieceDownloaded(int peer_id1, int peer_id2, int pieceindex, int numpieces)
         {
             String fName= haslogfile(peer_id1);
+            BufferedWriter writetofile;
             try
             {
-                FileWriter writer = new FileWriter(fName,true);
-                BufferedWriter writetofile = new BufferedWriter(writer);
+                writetofile= new BufferedWriter(new FileWriter(fName,true));
                 writetofile.write(printdate()+": Peer "+peer_id1+" has downloaded the piece "+pieceindex+" from Peer "+peer_id2+". Now the number of pieces it has is "+numpieces+".");
                 writetofile.newLine();
-                writetofile.close();
+
             }
             catch(IOException e)
             {
                 e.printStackTrace();
+            }
+            finally {
+                writetofile.close();
             }
         }
         public void fileDownloaded(int peer_id1)
         {
             String fName = "peer_" + peer_id1+"/log_peer_"+peer_id1+".log";
+            BufferedWriter writetofile;
             try
             {
-                FileWriter writer = new FileWriter(fName,true);
-                BufferedWriter writetofile = new BufferedWriter(writer);
+                writetofile = new BufferedWriter(new FileWriter(fName,true));
                 writetofile.write(printdate()+": Peer "+peer_id1+" has downloaded the complete file.");
                 writetofile.newLine();
-                writetofile.close();
+                //writetofile.close();
             }
             catch(IOException e)
             {
                 e.printStackTrace();
             }
+            finally {
+                writetofile.close();
+            }
         }
     }
+
+
 
